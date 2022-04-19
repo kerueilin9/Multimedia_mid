2b11e9f1ea48195491ad10142918ae3dac62fdc1
DESKTOP-QQQEDJS\kerueilin9 <kerueilin9>
2022-04-20 07:21:14 +0800

    1

--- mid_project/untitled1.py
+++ mid_project/untitled1.py
@@ -5,10 +5,20 @@ from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
+img2 = cv2.imread("2.jpg")
+img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+img2 = cv2.resize(img2, (28, 28)).reshape(1, -1)
+ret, img2 = cv2.threshold(img2, 187, 255, cv2.THRESH_BINARY)
+# =============================================================================
+# img2 = img2.reshape(28, 28)
+# cv2.imshow("123",img2)
+# cv2.waitKey(0)
+# =============================================================================
+img2 = img2[0]
+print(img2)
 test_data = np.uint8(np.loadtxt("label.txt"))
 
 video = cv2.VideoCapture("test_dataset.avi")
-img2 = cv2.imread("2.jpg")
 success = True
 
 video_data = []
@@ -23,7 +33,6 @@ while(success):
     video_data.append(image)
     
 data = np.array(video_data).reshape(len(video_data), -1)
-
 # =============================================================================
 # 
 # _, axes = plt.subplots(nrows = 1, ncols = 5, figsize = (20, 10))
@@ -44,8 +53,9 @@ data = np.array(video_data).reshape(len(video_data), -1)
 clf = KNeighborsClassifier()
 x_train, x_test, y_train, y_test = train_test_split(data, test_data, test_size=0.05)
 
-print(x_train)
+#print(x_train)
 clf.fit(x_train, y_train)
-MyPredict = clf.predict(x_test)
+MyPredict = clf.predict([img2])
+print(MyPredict)
 
 
